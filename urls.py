865caa92b763fd21637a58370497c24e88e0f426
Zelvuska <zelvuska@matfyz.cz>
2010-11-05 17:39:41 +0100

    Fixes after merging...

--- urls.py
+++ urls.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
-<<<<<<< Updated upstream
 from django.conf.urls.defaults import patterns, url, include
 from django.contrib import admin
 from django.views.decorators.csrf import csrf_exempt
 from django.views.i18n import set_language
-=======
 from datetime import date
 from people.models import Grant, Thesis
 from django.db.models import Q
->>>>>>> Stashed changes
 
 admin.autodiscover()
 
@@ -26,18 +23,15 @@ urlpatterns = patterns('',
     url(r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'front_page.html'}, name="homepage"),
     # pages
     url(r'^people/', include('people.urls')),
-<<<<<<< Updated upstream
     # admin
     url(r'^admin/', include(admin.site.urls)),
     # languages
     url(r'^setlang/$', csrf_exempt(set_language), name="set_language"),
     # media
     url(r'^%s(?P<path>.*)$' % settings.MEDIA_URL[1:], 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
-=======
     url(r'^articles/$', 'people.views.article_list'),
     url(r'^articles/([0-9]{4})/$', 'people.views.article_list'),
-    url(r'^grants/$', 'django.views.generic.list_detail.object_list', grants),
+#    url(r'^grants/$', 'django.views.generic.list_detail.object_list', grants),
     url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^thesis_defend/$', 'django.views.generic.list_detail.object_list', thesis),
->>>>>>> Stashed changes
 )
