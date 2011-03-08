f7f35da7b6d56d9d179d39345001b622ce76285b
Zelvuska <zelvuska@matfyz.cz>
2011-03-08 16:08:23 +0100

    Help texts on grants and persons. Verbose name on Grants.

--- people/models/person.py
+++ people/models/person.py
@@ -48,13 +48,13 @@ class Human(models.Model):
 class Person(models.Model):
     # functional fields
     is_active = models.BooleanField(default=True)
-    human = models.ForeignKey(Human, blank=True, null=True) #required if type != None
+    human = models.ForeignKey(Human, blank=True, null=True, help_text=_('Only for staff or students. Do not fill in for others!')) #required if type != None
     type = models.CharField(max_length=10, blank=True, null=True, choices=PERSON_TYPES)
     advisor = models.ForeignKey('Person', related_name='student', blank=True, null=True)
-    place = models.ForeignKey(Place, blank=True, null=True)
+    place = models.ForeignKey(Place, blank=True, null=True, help_text=_('Only required for grant applicants/co-applicants or staff.'))
     # data fields
     prefix = models.CharField(max_length=20, blank=True, null=True)
-    first_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50, help_text=_('Only first letter is required for article authors. In case of multiple first names, fill them separated by space.'))
     last_name = models.CharField(max_length=50)
     suffix = models.CharField(max_length=20, blank=True, null=True)
     class_year = models.SmallIntegerField(blank=True, null=True)
@@ -67,7 +67,7 @@ class Person(models.Model):
         )
 
     def __unicode__(self):
-        return u"%s %s (%s)" % (self.last_name, self.first_name, self.human)
+        return u"%s %s (%s)" % (self.last_name, self.first_name, self.type)
 
     def clean(self):
         if self.human:
