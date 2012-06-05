2b0463c2ab14f0d6a9c8df2295159c25953aa221
Zelvuska <zelvuska@matfyz.cz>
2012-06-05 15:41:22 +0200

    Added MEDIA_URL to both settings files to handle user files.

--- settings_ofb.py
+++ settings_ofb.py
@@ -61,6 +61,7 @@ STATIC_ROOT = '/home/.../production/static'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
 
 STATIC_DIRS = (
