b2ed4654eb352f2de5b47c25ab559fa36e2aa7b5
Vlastimil Zíma <vlastimil.zima@gmail.com>
2014-02-07 21:12:02 +0100

    Delete custom error handlers
     * Using defualt handlers should be good enough.

--- fuuk/utils.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponseServerError
-from django.template import loader, RequestContext
-
-
-def handler500(request):
-    t = loader.get_template('500.html')
-    return HttpResponseServerError(t.render(RequestContext(request)))
