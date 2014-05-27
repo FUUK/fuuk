0e5d84788bd74a43559ac7c3c650cbbef48279aa
Zelvuska <zelvuska@matfyz.cz>
2014-05-27 14:42:16 +0200

    Hotfix for personal photos and OPPO url redirect

--- fuuk/urls_oppo.py
+++ fuuk/urls_oppo.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls.defaults import patterns, url, include
+from django.core.urlresolvers import reverse_lazy
 
 urlpatterns = patterns('',
     # homepage redirect
-    url(r'^$', 'django.views.generic.simple.redirect_to', {'url': '/thesis_offer'}),
+    url(r'^$', 'django.views.generic.simple.redirect_to', {'url': reverse_lazy('flatpage', args=('thesis_offer/',))}),
     # pages
     url(r'^', include('fuuk.urls')),
 )
