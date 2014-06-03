aa9f767a9dfe18c8b064f8bfdc17944a45698dad
Vlastimil Zíma <vlastimil.zima@gmail.com>
2014-06-03 13:53:07 +0200

    Clean gettext files
     * Remove binary gettext files from version control.
     * Sort source gettext file.
     * Compile gettext file on build.

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
+import subprocess
+import sys
+
+from distutils import log
+from distutils.command.build_py import build_py
 from distutils.core import setup
+from distutils.dep_util import newer
 
 from fuuk import __version__
 
@@ -15,6 +21,31 @@ PACKAGE_DATA = {'fuuk': ['locale/cs/LC_MESSAGES/*', 'templates/*.html', 'templat
                 'fuuk.people': ['fixtures/*.yaml', 'static/css/*', 'static/img/*', 'static/js/*']}
 
 
+class i18n_build_py(build_py):
+    """
+    Distuutils `build_py` command which compiles also gettext files.
+    """
+    def byte_compile(self, files):
+        build_py.byte_compile(self, files)
+
+        # Skip compiling
+        if sys.dont_write_bytecode:
+            return
+
+        for pofile in files:
+            if pofile[-3:] != ".po":
+                # Don't compile anything else
+                continue
+            mofile = pofile[:-3] + '.mo'
+            if self.force or newer(pofile, mofile):
+                log.info("byte-compiling %s to %s", pofile, mofile)
+                if not self.dry_run:
+                    subprocess.check_call(['msgfmt', '-o', mofile, pofile])
+            else:
+                log.debug("skipping byte-compilation of %s to %s",
+                          pofile, mofile)
+
+
 def main():
     setup(name='Web FUUK',
           version=__version__,
@@ -22,7 +53,8 @@ def main():
           author_email='vlastimil.zima@gmail.com',
           description='Web of Institute of Physics at Charles University',
           packages=PACKAGES,
-          package_data=PACKAGE_DATA)
+          package_data=PACKAGE_DATA,
+          cmdclass={'build_py': i18n_build_py})
 
 
 if __name__ == '__main__':
